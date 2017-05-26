9ab97590b1fa28b0673a35f67734b3f9038625f7
cwainner <brennenburg@gmail.com>
2017-05-26 12:40:29 -0700

    Setup basic unit testing framework

---
+++ app/src/test/java/com/cwainner/chris/recipecentral/MainActivityTest.java
@@ -0,0 +1,31 @@
+package com.cwainner.chris.recipecentral;
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.*;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup(){
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateMainHeader(){
+        TextView appNameTextView = (TextView) activity.findViewById(R.id.mainHeader);
+        assertTrue("Welcome to Recipe Central!".equals(appNameTextView.getText().toString()));
+    }
+}
