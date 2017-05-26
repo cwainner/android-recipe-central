72039c40febe9b9f86a6d29861e6ec7fe73d7ff2
cwainner <brennenburg@gmail.com>
2017-05-26 12:14:16 -0700

    Add new font to main header

--- app/src/main/java/com/cwainner/chris/recipecentral/MainActivity.java
+++ app/src/main/java/com/cwainner/chris/recipecentral/MainActivity.java
@@ -1,13 +1,33 @@
 package com.cwainner.chris.recipecentral;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.aboutButton) Button aboutButton;
+    @Bind(R.id.contactButton) Button contactButton;
+    @Bind(R.id.getRecipesButton) Button getRecipesButton;
+    @Bind(R.id.mainHeader) TextView mainHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface quicksandFont = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Bold.otf");
+        mainHeader.setTypeface(quicksandFont);
+    }
+
+    @Override
+    public void onClick(View v){
+
     }
 }
