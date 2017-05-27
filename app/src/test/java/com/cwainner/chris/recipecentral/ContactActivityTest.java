984cbed63c2c6dc64d34196b1ef06e8ffe57cc33
cwainner <brennenburg@gmail.com>
2017-05-27 14:11:06 -0700

    Add contact activity test with basic framework

---
+++ app/src/test/java/com/cwainner/chris/recipecentral/ContactActivityTest.java
@@ -0,0 +1,32 @@
+package com.cwainner.chris.recipecentral;
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+
+import static org.junit.Assert.*;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class ContactActivityTest {
+    private ContactActivity activity;
+
+    @Before
+    public void setup(){
+        activity = Robolectric.setupActivity(ContactActivity.class);
+    }
+
+    @Test
+    public void validateContactHeader(){
+        TextView contactHeaderView = (TextView) activity.findViewById(R.id.contactHeader);
+        assertTrue("Contact".equals(contactHeaderView.getText().toString()));
+    }
+}
