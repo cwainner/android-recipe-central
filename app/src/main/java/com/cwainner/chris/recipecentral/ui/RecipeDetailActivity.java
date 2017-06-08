2ac26d767d8d4f38fb171b0023cfe4dda111c9a6
Chris Wainner <chris.jwainner@gmail.com>
2017-06-08 16:40:33 -0700

    Remove tab pager and replace with a full detail page for each recipe

--- app/src/main/java/com/cwainner/chris/recipecentral/ui/RecipeDetailActivity.java
+++ app/src/main/java/com/cwainner/chris/recipecentral/ui/RecipeDetailActivity.java
@@ -1,8 +1,10 @@
 package com.cwainner.chris.recipecentral.ui;
 
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import com.cwainner.chris.recipecentral.R;
 import com.cwainner.chris.recipecentral.adapters.RecipePagerAdapter;
@@ -16,7 +18,7 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class RecipeDetailActivity extends AppCompatActivity {
-    @Bind(R.id.viewPager) ViewPager viewPager;
+    @Bind(R.id.recipeDetailFrameLayout) FrameLayout frameLayout;
     private RecipePagerAdapter adapterViewPager;
     ArrayList<Recipe> recipes = new ArrayList<>();
 
@@ -30,7 +32,13 @@ public class RecipeDetailActivity extends AppCompatActivity {
         int startingPosition = getIntent().getIntExtra("position", 0);
 
         adapterViewPager = new RecipePagerAdapter(getSupportFragmentManager(), recipes);
-        viewPager.setAdapter(adapterViewPager);
-        viewPager.setCurrentItem(startingPosition);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        RecipeDetailFragment recipeDetailFragment = new RecipeDetailFragment();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("recipe", Parcels.wrap(recipes.get(startingPosition)));
+        recipeDetailFragment.setArguments(bundle);
+        fragmentTransaction.add(R.id.recipeDetailFrameLayout, recipeDetailFragment);
+        fragmentTransaction.commit();
     }
 }
