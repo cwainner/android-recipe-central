e28c656319e5dc73ae73e0c6e6c0f056b918fa47
cwainner <brennenburg@gmail.com>
2017-06-10 16:44:07 -0700

    Add logic to switch between login and create account fragments on LoginActivity

--- app/src/main/java/com/cwainner/chris/recipecentral/ui/LoginActivity.java
+++ app/src/main/java/com/cwainner/chris/recipecentral/ui/LoginActivity.java
@@ -1,9 +1,10 @@
 package com.cwainner.chris.recipecentral.ui;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
-import android.widget.FrameLayout;
 import android.widget.ToggleButton;
 
 import com.cwainner.chris.recipecentral.R;
@@ -13,21 +14,34 @@ import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
     @Bind(R.id.createAccountButton) ToggleButton createAccountButton;
-    @Bind(R.id.loginFrameLayout) FrameLayout loginFrameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        final FragmentManager fm = getSupportFragmentManager();
+        LoginFragment initialLoginFragment = new LoginFragment();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.loginFrameLayout, initialLoginFragment);
+        fragmentTransaction.commit();
 
         createAccountButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
                     // Load create account fragment
+                    CreateAccountFragment createAccountFragment = new CreateAccountFragment();
+
+                    FragmentTransaction fragmentTransaction = fm.beginTransaction();
+                    fragmentTransaction.replace(R.id.loginFrameLayout, createAccountFragment);
+                    fragmentTransaction.commit();
                 } else {
                     // Load login fragment
+                    LoginFragment loginFragment = new LoginFragment();
+                    FragmentTransaction fragmentTransaction = fm.beginTransaction();
+                    fragmentTransaction.replace(R.id.loginFrameLayout, loginFragment);
+                    fragmentTransaction.commit();
                 }
             }
         });
