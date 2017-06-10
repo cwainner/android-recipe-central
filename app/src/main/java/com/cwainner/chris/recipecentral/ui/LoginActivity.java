ec9c45d436531532344c57937c77b95e17e6fd97
cwainner <brennenburg@gmail.com>
2017-06-10 16:19:33 -0700

    Add login fragment with input fields and login button

--- app/src/main/java/com/cwainner/chris/recipecentral/ui/LoginActivity.java
+++ app/src/main/java/com/cwainner/chris/recipecentral/ui/LoginActivity.java
@@ -2,6 +2,8 @@ package com.cwainner.chris.recipecentral.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.FrameLayout;
 import android.widget.ToggleButton;
 
 import com.cwainner.chris.recipecentral.R;
@@ -11,11 +13,23 @@ import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
     @Bind(R.id.createAccountButton) ToggleButton createAccountButton;
+    @Bind(R.id.loginFrameLayout) FrameLayout loginFrameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        createAccountButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    // Load create account fragment
+                } else {
+                    // Load login fragment
+                }
+            }
+        });
     }
 }
