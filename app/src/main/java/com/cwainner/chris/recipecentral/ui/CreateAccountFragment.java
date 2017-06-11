145a4b00c2bcad0923449343cf03ffb3c85cf4d8
cwainner <brennenburg@gmail.com>
2017-06-10 18:05:42 -0700

    Add form inputs to create account fragment, add validation for passwords

--- app/src/main/java/com/cwainner/chris/recipecentral/ui/CreateAccountFragment.java
+++ app/src/main/java/com/cwainner/chris/recipecentral/ui/CreateAccountFragment.java
@@ -1,19 +1,28 @@
 package com.cwainner.chris.recipecentral.ui;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cwainner.chris.recipecentral.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class CreateAccountFragment extends Fragment {
 
+public class CreateAccountFragment extends Fragment implements View.OnClickListener{
+
+    private EditText emailEditText;
+    private EditText nameEditText;
+    private EditText passwordEditText;
+    private EditText confirmPasswordEditText;
+    private Button createAccountButton;
+
+    private Context context;
 
     public CreateAccountFragment() {
         // Required empty public constructor
@@ -24,7 +33,35 @@ public class CreateAccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_create_account, container, false);
+
+        context = view.getContext();
+
+        emailEditText = (EditText) view.findViewById(R.id.emailEditText);
+        nameEditText = (EditText) view.findViewById(R.id.nameEditText);
+        passwordEditText = (EditText) view.findViewById(R.id.passwordEditText);
+        confirmPasswordEditText = (EditText) view.findViewById(R.id.confirmPasswordEditText);
+        createAccountButton = (Button) view.findViewById(R.id.createAccountSubmitButton);
+
+        createAccountButton.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v == createAccountButton){
+            createNewUser();
+        }
+    }
+
+    private void createNewUser(){
+        final String name = nameEditText.getText().toString().trim();
+        final String email = emailEditText.getText().toString().trim();
+        final String password = passwordEditText.getText().toString().trim();
+        final String confirmPassword = confirmPasswordEditText.getText().toString().trim();
+        if((password.length() < 6) || !(password.equals(confirmPassword))){
+            Toast.makeText(context, "Please enter a password that is at least 6 characters long. Both passwords much also match.", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
--- app/src/main/java/com/cwainner/chris/recipecentral/ui/RecipeListActivity.java
+++ app/src/main/java/com/cwainner/chris/recipecentral/ui/RecipeListActivity.java
@@ -8,10 +8,13 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
+import com.cwainner.chris.recipecentral.Constants;
 import com.cwainner.chris.recipecentral.R;
 import com.cwainner.chris.recipecentral.adapters.RecipeListAdapter;
 import com.cwainner.chris.recipecentral.models.Recipe;
 import com.cwainner.chris.recipecentral.services.RecipeService;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -23,17 +26,24 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class RecipeListActivity extends AppCompatActivity {
+
     public static final String TAG = RecipeListActivity.class.getSimpleName();
 
     @Bind(R.id.recipesHeader) TextView recipesHeader;
     @Bind(R.id.recyclerView) RecyclerView recyclerView;
 
     private RecipeListAdapter adapter;
+    private DatabaseReference searchedReference;
 
     public ArrayList<Recipe> recipes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        searchedReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_SEARCHED);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
         ButterKnife.bind(this);
