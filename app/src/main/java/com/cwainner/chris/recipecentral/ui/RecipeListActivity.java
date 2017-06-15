14de91896076ffaa266812055c3c4f1e2dca4fbf
Chris Wainner <chris.jwainner@gmail.com>
2017-06-15 11:37:51 -0700

    Create RecipeListFragment and move content from RecipeListActivity to it to create a flexible UI base with fragments

--- app/src/main/java/com/cwainner/chris/recipecentral/ui/RecipeListActivity.java
+++ app/src/main/java/com/cwainner/chris/recipecentral/ui/RecipeListActivity.java
@@ -1,93 +1,15 @@
 package com.cwainner.chris.recipecentral.ui;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Typeface;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.widget.TextView;
 
-import com.cwainner.chris.recipecentral.Constants;
 import com.cwainner.chris.recipecentral.R;
-import com.cwainner.chris.recipecentral.adapters.RecipeListAdapter;
-import com.cwainner.chris.recipecentral.models.Recipe;
-import com.cwainner.chris.recipecentral.services.RecipeService;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.io.IOException;
-import java.util.ArrayList;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Response;
 
 public class RecipeListActivity extends AppCompatActivity {
 
-    public static final String TAG = RecipeListActivity.class.getSimpleName();
-
-    @Bind(R.id.recipesHeader) TextView recipesHeader;
-    @Bind(R.id.recyclerView) RecyclerView recyclerView;
-
-    private RecipeListAdapter adapter;
-    private DatabaseReference searchedReference;
-    private SharedPreferences sharedPreferences;
-    private String searched;
-
-    public ArrayList<Recipe> recipes = new ArrayList<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        searchedReference = FirebaseDatabase
-                .getInstance()
-                .getReference()
-                .child(Constants.FIREBASE_CHILD_SEARCHED);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
-        ButterKnife.bind(this);
-
-        // Get Parameters from intent
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        searched = sharedPreferences.getString(Constants.PREFERENCES_SEARCHED_KEY, null);
-
-        // Set header font
-        Typeface quicksandFont = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Bold.otf");
-        recipesHeader.setTypeface(quicksandFont);
-
-        // Get recipes from API
-        getRecipes(searched);
-
-    }
-
-    private void getRecipes(String ingredients){
-        final RecipeService recipeService = new RecipeService();
-        recipeService.findRecipes(ingredients, new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) {
-                recipes = recipeService.processResults(response);
-
-                RecipeListActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        adapter = new RecipeListAdapter(getApplicationContext(), recipes);
-                        recyclerView.setAdapter(adapter);
-                        recyclerView.setLayoutManager(new LinearLayoutManager(RecipeListActivity.this));
-                        recyclerView.setHasFixedSize(true);
-                    }
-                });
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/cwainner/chris/recipecentral/ui/RecipeListFragment.java
@@ -0,0 +1,143 @@
+package com.cwainner.chris.recipecentral.ui;
+
+
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.cwainner.chris.recipecentral.Constants;
+import com.cwainner.chris.recipecentral.R;
+import com.cwainner.chris.recipecentral.adapters.RecipeListAdapter;
+import com.cwainner.chris.recipecentral.models.Recipe;
+import com.cwainner.chris.recipecentral.services.RecipeService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RecipeListFragment extends Fragment {
+    @Bind(R.id.recipesHeader) TextView recipesHeader;
+    @Bind(R.id.recyclerView) RecyclerView recyclerView;
+
+    private RecipeListAdapter adapter;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private String searched;
+
+    public ArrayList<Recipe> recipes = new ArrayList<>();
+
+    public RecipeListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        // Get shared preferences
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        editor = sharedPreferences.edit();
+
+        // Set header font
+        Typeface quicksandFont = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Quicksand-Bold.otf");
+        recipesHeader.setTypeface(quicksandFont);
+
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_recipe_list, container, false);
+        ButterKnife.bind(this, view);
+
+        searched = sharedPreferences.getString(Constants.PREFERENCES_SEARCHED_KEY, null);
+
+        if(searched != null){
+            getRecipes(searched);
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_search, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                addToSharedPreferences(query);
+                getRecipes(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        return super.onOptionsItemSelected(menuItem);
+    }
+
+    private void getRecipes(String ingredients){
+        final RecipeService recipeService = new RecipeService();
+        recipeService.findRecipes(ingredients, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                recipes = recipeService.processResults(response);
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter = new RecipeListAdapter(getActivity(), recipes);
+                        recyclerView.setAdapter(adapter);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                        recyclerView.setHasFixedSize(true);
+                    }
+                });
+            }
+        });
+    }
+
+    private void addToSharedPreferences(String query){
+        editor.putString(Constants.PREFERENCES_SEARCHED_KEY, query).apply();
+    }
+}
