01c98de7aa2da91657266c5c50720edd7141ee49
Chris Wainner <chris.jwainner@gmail.com>
2017-06-15 12:47:18 -0700

    Change RecipeList and SavedRecipeList layouts to use fragments

--- app/src/main/java/com/cwainner/chris/recipecentral/ui/RecipeListActivity.java
+++ app/src/main/java/com/cwainner/chris/recipecentral/ui/RecipeListActivity.java
@@ -10,6 +10,6 @@ public class RecipeListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipes);
+        setContentView(R.layout.activity_recipe_list);
     }
 }
--- app/src/main/java/com/cwainner/chris/recipecentral/ui/SavedRecipeListActivity.java
+++ app/src/main/java/com/cwainner/chris/recipecentral/ui/SavedRecipeListActivity.java
@@ -11,6 +11,6 @@ public class SavedRecipeListActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipes);
+        setContentView(R.layout.activity_saved_recipe_list);
     }
 }
